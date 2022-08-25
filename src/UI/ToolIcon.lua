903a254179897225737bd05d7142b68c2beccf96
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-08-25 18:06:42 -0400

    Create individual tool icon.

---
+++ src/UI/ToolIcon.lua
@@ -0,0 +1,160 @@
+--[[
+TheNexusAvenger
+
+Class for a tool icon.
+--]]
+
+local ToolIcon = {}
+ToolIcon.__index = ToolIcon
+
+
+
+--[[
+Creates the tool icon.
+--]]
+function ToolIcon.new(Parent: GuiObject, PositionX: number, PositionY: number)
+    --Create the object.
+    local self = {
+        Focused = false,
+        Players = game:GetService("Players"), --Stored to be replaced in unit tests.
+        TweenService = game:GetService("TweenService"), --Stored to be replaced in unit tests.
+        RelativePositionX = (PositionX * (math.sqrt(3) / 2)) + 0.5,
+        RelativePositionY = (PositionY * 0.75) + 0.5,
+    }
+    setmetatable(self, ToolIcon)
+
+    --Create the frames.
+    local Background = Instance.new("ImageLabel")
+    Background.BackgroundTransparency = 1
+    Background.AnchorPoint = Vector2.new(0.5, 0.5)
+    Background.Size = UDim2.new(0.9, 0, 0.9, 0)
+    Background.Position = UDim2.new(self.RelativePositionX, 0, self.RelativePositionY, 0)
+    Background.Image = "http://www.roblox.com/asset/?id=10708006436"
+    Background.ImageColor3 = Color3.new(0.1, 0.1, 0.1)
+    Background.ImageTransparency = 0.8
+    Background.Parent = Parent
+    self.Background = Background
+
+    local ToolText = Instance.new("TextLabel")
+    ToolText.BackgroundTransparency = 1
+    ToolText.AnchorPoint = Vector2.new(0.5, 0.5)
+    ToolText.Position = UDim2.new(0.5, 0, 0.5, 0)
+    ToolText.Size = UDim2.new(0.625, 0, 0.625, 0)
+    ToolText.Font = Enum.Font.SourceSans
+    ToolText.TextColor3 = Color3.new(1, 1, 1)
+    ToolText.Text = ""
+    ToolText.TextSize = (Background.AbsoluteSize.Y * 0.625) / 4
+    ToolText.TextWrapped = true
+    ToolText.Visible = false
+    ToolText.Parent = Background
+    self.ToolText = ToolText
+    Background:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
+        ToolText.TextSize = (Background.AbsoluteSize.Y * 0.625) / 4
+    end)
+
+    local ToolImage = Instance.new("ImageLabel")
+    ToolImage.BackgroundTransparency = 1
+    ToolImage.AnchorPoint = Vector2.new(0.5, 0.5)
+    ToolImage.Position = UDim2.new(0.5, 0, 0.5, 0)
+    ToolImage.Size = UDim2.new(0.625, 0, 0.625, 0)
+    ToolImage.Visible = false
+    ToolImage.Parent = Background
+    self.ToolImage = ToolImage
+
+    --Return the object.
+    return self
+end
+
+--[[
+Updates the color of the icon.
+--]]
+function ToolIcon:UpdateColor(): nil
+    --Update the text and icon.
+    if self.Tool then
+        if self.Tool.TextureId == "" then
+            self.ToolText.Visible = true
+            self.ToolImage.Visible = false
+            self.ToolText.Text = self.Tool.Name
+        else
+            self.ToolText.Visible = false
+            self.ToolImage.Visible = true
+            self.ToolImage.Image = self.Tool.TextureId
+        end
+    else
+        self.ToolText.Visible = false
+        self.ToolImage.Visible = false
+    end
+
+    --Determine the color, transparency, and size.
+    local Color = Color3.new(0.1, 0.1, 0.1)
+    local Transparency = (self.Tool and 0.5 or 0.8)
+    local Size = ((self.Tool and self.Focused and UDim2.new(1.1, 0, 1.1, 0)) or UDim2.new(0.9, 0, 0.9, 0))
+    if self.Tool then
+        if self.Players.LocalPlayer.Character and self.Players.LocalPlayer.Character == self.Tool.Parent then
+            if self.Focused then
+                Color = Color3.new(0.2, 1, 0.2)
+            else
+                Color = Color3.new(0, 0.7, 0)
+            end
+        else
+            if self.Focused then
+                Color = Color3.new(0.2, 0.2, 0.2)
+            end
+        end
+    end
+
+    --Set the properties.
+    self.TweenService:Create(self.Background, TweenInfo.new(0.1), {
+        Size = Size,
+        ImageColor3 = Color,
+        ImageTransparency = Transparency,
+    }):Play()
+end
+
+--[[
+Sets the tool for the icon.
+--]]
+function ToolIcon:SetTool(Tool: Tool): nil
+    --Disconnect the tool events.
+    self.Tool = Tool
+    if self.ToolEvents then
+        for _, Event in self.ToolEvents do
+            Event:Disconnect()
+        end
+        self.ToolEvents = nil
+    end
+    if not Tool then return end
+
+    --Connect the tool events.
+    self.ToolEvents = {}
+    table.insert(self.ToolEvents, Tool.Changed:Connect(function(PropertyName)
+        if PropertyName ~= "Name" and PropertyName ~= "TextureId" and PropertyName ~= "Parent" then return end
+        self:UpdateColor()
+    end))
+    self:UpdateColor()
+end
+
+--[[
+Sets the icon as focused.
+--]]
+function ToolIcon:SetFocused(Focused: boolean): nil
+    self.Focused = Focused
+    self:UpdateColor()
+end
+
+--[[
+Destroys the tool icon.
+--]]
+function ToolIcon:Destroy()
+    self.Background:Destroy()
+    if self.ToolEvents then
+        for _, Event in self.ToolEvents do
+            Event:Disconnect()
+        end
+        self.ToolEvents = nil
+    end
+end
+
+
+
+return ToolIcon
---
+++ test/UI/ToolIcon.spec.lua
@@ -0,0 +1,186 @@
+--[[
+TheNexusAvenger
+
+Tests for the ToolIcon class.
+--]]
+
+local ServerScriptService = game:GetService("ServerScriptService")
+local ToolIcon = require(ServerScriptService:WaitForChild("MainModule"):WaitForChild("NexusVRBackpack"):WaitForChild("UI"):WaitForChild("ToolIcon"))
+
+return function()
+    --Create the test tool icon and mock services.
+    local TestToolIcon = nil
+    beforeEach(function()
+        TestToolIcon = ToolIcon.new(nil, 0, 0)
+        TestToolIcon.Players = {
+            LocalPlayer = {
+                Character = Instance.new("Model"),
+            },
+        }
+        TestToolIcon.TweenService = {
+            Create = function(self, Ins, _, Properties)
+                self.LastProperties = Properties
+                for Name, Value in Properties do
+                    Ins[Name] = Value
+                end
+                return {
+                    Play = function() end
+                }
+            end,
+            shouldHaveSet = function(Color, Size, Transparency)
+                expect(TestToolIcon.TweenService.LastProperties.ImageColor3).to.equal(Color)
+                expect(TestToolIcon.TweenService.LastProperties.Size).to.equal(Size)
+                expect(TestToolIcon.TweenService.LastProperties.ImageTransparency).to.be.near(Transparency)
+            end
+        }
+    end)
+    afterEach(function()
+        TestToolIcon:Destroy()
+        TestToolIcon = nil
+    end)
+
+    --Run the tests.
+    describe("ToolIcon with no tool", function()
+        it("should have default properties", function()
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.8)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+        end)
+
+        it("should not change when focused", function()
+            TestToolIcon:SetFocused(true)
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.8)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.1, 0.1, 0.1), UDim2.new(0.9, 0, 0.9, 0), 0.8)
+        end)
+
+        it("should not change when unfocused", function()
+            TestToolIcon:SetFocused(true)
+            TestToolIcon:SetFocused(false)
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.8)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.1, 0.1, 0.1), UDim2.new(0.9, 0, 0.9, 0), 0.8)
+        end)
+    end)
+
+    describe("ToolIcon with a tool with no icon", function()
+        it("should show the tool name", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            TestToolIcon:SetTool(Tool)
+
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            expect(TestToolIcon.ToolText.Text).to.equal("TestName")
+            expect(TestToolIcon.ToolText.Visible).to.equal(true)
+            expect(TestToolIcon.ToolImage.Visible).to.equal(false)
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.1, 0.1, 0.1), UDim2.new(0.9, 0, 0.9, 0), 0.5)
+        end)
+
+        it("should change when focused", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            TestToolIcon:SetTool(Tool)
+
+            TestToolIcon:SetFocused(true)
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.2, 0.2, 0.2))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(1.1, 0, 1.1, 0))
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.2, 0.2, 0.2), UDim2.new(1.1, 0, 1.1, 0), 0.5)
+        end)
+
+        it("should become green when equipped", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            TestToolIcon:SetTool(Tool)
+            Tool.Parent = TestToolIcon.Players.LocalPlayer.Character
+            task.wait()
+
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0, 0.7, 0))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0, 0.7, 0), UDim2.new(0.9, 0, 0.9, 0), 0.5)
+
+            TestToolIcon:SetFocused(true)
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.2, 1, 0.2))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(1.1, 0, 1.1, 0))
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.2, 1, 0.2), UDim2.new(1.1, 0, 1.1, 0), 0.5)
+        end)
+
+        it("should reset when unequipped", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            Tool.Parent = TestToolIcon.Players.LocalPlayer.Character
+            TestToolIcon:SetTool(Tool)
+            Tool.Parent = nil
+            task.wait()
+
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.1, 0.1, 0.1), UDim2.new(0.9, 0, 0.9, 0), 0.5)
+        end)
+
+        it("should update the tool name", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            TestToolIcon:SetTool(Tool)
+            expect(TestToolIcon.ToolText.Text).to.equal("TestName")
+
+            Tool.Name = "NewName"
+            task.wait()
+            expect(TestToolIcon.ToolText.Text).to.equal("NewName")
+        end)
+
+        it("should change to an image", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            TestToolIcon:SetTool(Tool)
+            expect(TestToolIcon.ToolText.Text).to.equal("TestName")
+
+            Tool.TextureId = "TestIcon"
+            task.wait()
+            expect(TestToolIcon.ToolImage.Image).to.equal("TestIcon")
+            expect(TestToolIcon.ToolText.Visible).to.equal(false)
+            expect(TestToolIcon.ToolImage.Visible).to.equal(true)
+        end)
+    end)
+
+    describe("ToolIcon with a tool with an icon", function()
+        it("should show the tool icon", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            Tool.TextureId = "TestIcon"
+            TestToolIcon:SetTool(Tool)
+
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            expect(TestToolIcon.ToolImage.Image).to.equal("TestIcon")
+            expect(TestToolIcon.ToolText.Visible).to.equal(false)
+            expect(TestToolIcon.ToolImage.Visible).to.equal(true)
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.1, 0.1, 0.1), UDim2.new(0.9, 0, 0.9, 0), 0.5)
+        end)
+
+        it("should change to text", function()
+            local Tool = Instance.new("Tool")
+            Tool.Name = "TestName"
+            Tool.TextureId = "TestIcon"
+            TestToolIcon:SetTool(Tool)
+            Tool.TextureId = ""
+            task.wait()
+
+            expect(TestToolIcon.Background.ImageTransparency).to.be.near(0.5)
+            expect(TestToolIcon.Background.ImageColor3).to.equal(Color3.new(0.1, 0.1, 0.1))
+            expect(TestToolIcon.Background.Size).to.equal(UDim2.new(0.9, 0, 0.9, 0))
+            expect(TestToolIcon.ToolText.Text).to.equal("TestName")
+            expect(TestToolIcon.ToolText.Visible).to.equal(true)
+            expect(TestToolIcon.ToolImage.Visible).to.equal(false)
+            TestToolIcon.TweenService.shouldHaveSet(Color3.new(0.1, 0.1, 0.1), UDim2.new(0.9, 0, 0.9, 0), 0.5)
+        end)
+    end)
+end
