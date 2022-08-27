ac808bdaa1f535c36e1898f8fa034f60eef3c1d2
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-08-27 00:12:19 -0400

    Make loader a function.

--- place/ServerScriptService/MainModule.lua
+++ place/ServerScriptService/MainModule.lua
@@ -8,10 +8,9 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 
 
---Copy NexusVRBackpack to ReplicatedStorage if it doesn't exist already.
---Client setup is up to Nexus VR Character Model or the game creator.
-if ReplicatedStorage:FindFirstChild("NexusVRBackpack") then return end
-script:WaitForChild("NexusVRBackpack").Parent = ReplicatedStorage
-
---Return true so that there are no errors.
-return true
+return function()
+    --Copy NexusVRBackpack to ReplicatedStorage if it doesn't exist already.
+    --Client setup is up to Nexus VR Character Model or the game creator.
+    if ReplicatedStorage:FindFirstChild("NexusVRBackpack") then return end
+    script:WaitForChild("NexusVRBackpack").Parent = ReplicatedStorage
+end
